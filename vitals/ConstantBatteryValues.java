f5574ecd8c169e96a4d09895b99114e15c73835a
Bapugouda Gulabal <38274563+bapugouda-gulabal@users.noreply.github.com>
2021-03-11 14:02:15 +0530

    Add files via upload

---
+++ vitals/ConstantBatteryValues.java
@@ -0,0 +1,11 @@
+package vitals;
+
+public class ConstantBatteryValues {
+	
+	public static final float LOWER_TEMPERATURE = 0;
+	public static final float HIGHER_TEMPERATURE = 45;
+	public static final float LOWER_SOC = 20;
+	public static final float HIGHER_SOC = 80;
+	public static final float HIGHER_CHARGERATE = (float) 0.8;
+		
+}
